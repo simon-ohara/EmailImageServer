27e69d29e436a255678485431eea8d67aac5b738
Simon O'Hara <simon.ohara@sage.com>
2011-12-20 14:19:52 +0000

    rails build

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EmailImageServer::Application.config.secret_token = '814a2860476d9fb67771ce92f5bf05b639c155507d9bcdee173b311581c148694bc26a02b656bc243331f1c1f70674fb0460cf75d65ff02c56e5a1caa41d0cc0'
